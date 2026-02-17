529fc9c870a54624a77715591138783b9f4d9d51
Jeepik <pashka.pkj.112@gmail.com>
2026-02-17 21:37:55 +0300

    Lesson 3 Инкапсуляция в ts

---
+++ notes/index.ts
@@ -0,0 +1,92 @@
+// Lesson 3
+// Инкапсуляция
+
+
+
+
+// class User {
+//     private name: string;
+//     private age: number;
+
+//     constructor(userName: string, userAge: number) {
+//         this.name = userName; 
+//         this.age = userAge;
+//     }
+
+//     publicgreet() {
+//         console.log('Привет, я ${this.name}, мне ${this.age} лет.');
+//     }
+// }
+
+// const alex = new User("Alex", 25);
+// const vika = new User("Vika", 29);
+
+// alex.greet();
+// vika.greet();
+
+
+
+
+// interface IUser {
+//     name: string;
+//     age: number;
+//     sayHello(): string;
+// }
+
+// class User implements IUser {
+//     public name: string;
+//     public age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     sayHello(): string {
+//         return `Привет, меня зовут ${this.name}, мне ${this.age} лет.`
+//     }
+// }
+
+
+
+
+class BankAccount {
+    private _balance: number;
+    private _owner: string;
+
+    constructor(balanc: number, owner: string) {
+        this._balance = balanc;
+        this._owner = owner;
+    }
+
+    public get balance(): number {
+        return this._balance;
+    }
+
+
+
+    public deposit(amount: number) {
+        if (amount <= 0) {
+            console.log("Ошибка: сумма должна быть больше 0.");
+            return;
+        }
+
+        this._balance += amount;
+        console.log(`${this._owner}, ваш баланс пополнен на ${amount}. Текущий баланс: ${this._balance}`);
+    }
+
+    public withdraw(amount: number) {
+        if (amount > this._balance) {
+            console.log("Ошибка: сумма должна быть меньше или равна текущему балансу.");
+            return;
+        }
+
+        this._balance -= amount;
+        console.log(`${this._owner}, ваш баланс снят на ${amount}. Текущий баланс: ${this._balance}`);
+    }      
+}
+
+const myAccount = new BankAccount(0, "Alex");
+
+myAccount.balance = 1001;
+
